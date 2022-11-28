0d202e005ba49da4f261b2c14a05d91146052191
ne_mene21 <73829851+nemene21@users.noreply.github.com>
2022-11-28 07:31:06 +0100

    end animation

--- generator.gd
+++ generator.gd
@@ -1,12 +1,13 @@
 extends StaticBody2D
 
 var stress = 50
+var broken = false
 
 func _process(delta):
 	
 	stress += delta
 	
-	if stress > 50:
+	if stress > 50 and !broken:
 		
 		var anim = (stress - 50) / 50
 		
@@ -17,8 +18,37 @@ func _process(delta):
 		
 		$Particles2D.emitting = true
 		
+		if stress > 100:
+			
+			$Camera2D.shake(18, 50, 0.15)
+			
+			$AnimationStarter.start()
+			
+			broken = true
+			
+			$Camera2D.global_position = Globals.player.get_node("Camera").global_position
+			
+			$Tween.interpolate_property(
+				$Camera2D,
+				"global_position",
+				$Camera2D.global_position,
+				Vector2(0, -15),
+				1.5,
+				Tween.TRANS_CUBIC,
+				Tween.EASE_IN_OUT
+			)
+			
+			$Tween.start()
+			
+			$Camera2D.current = true
+		
 	else:
 		
 		$Particles2D.emitting = false
 		
 		$Particles2D.modulate.a = 0
+
+
+func _on_AnimationStarter_timeout():
+	
+	$AnimationPlayer.play("explode")
